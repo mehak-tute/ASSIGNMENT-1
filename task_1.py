723b0fb367179d8b19f6847ce4ba0c2d14be8b86
mehak-tute <mehakpreetkaur1086@gmail.com>
2025-06-18 19:17:08 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,19 @@
+#TASK 1 :Perform Basic Mathematical Operations
+
+x=input("Enter the first number: ")
+x=float(x)
+
+y=input("Enter the secoond number: ")
+y=float(y)
+
+add=x+y
+print("Addition: ", add)
+
+sub=x-y
+print("Subtraction: ", sub)
+
+mul=x*y
+print("Multiplication: ", mul)
+
+div=x/y
+print("Division: ", div)
---
+++ task_2.py
@@ -0,0 +1,9 @@
+#TASK 2:Create a Personalized Greeting
+
+f_name=input("Enter your first name: ")
+
+l_name=input("Enter your last name: ")
+
+full_name= f_name + " " + l_name
+
+print("Hello,", full_name + "! Welcome to the Python program.")
